d7a7476cb0d1ad2062c8916a12843590888771db
Saara Rautiainen <saara.a.rautiainen@outlook.com>
2017-12-05 10:52:32 +0100

    Added text animation with jQuery

--- clientside.js
+++ clientside.js
@@ -1,10 +1,41 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    function onNewContent() {
+         $("#screen").scrollTop($("#screen-text").height());
+      }
+    	function randomIntFromInterval(min,max)
+	{
+	    return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
+	//writing function
+	function typeWrite(span){
+
+	  $('#'+ span).addClass('cursor')
+	  var text = $('#'+ span).text();
+	  var randInt = 0
+	  for (var i = 0; i < text.length; i++) {
+	    randInt += parseInt(randomIntFromInterval(0,40));
+	    var typing = setTimeout(function(y){
+	      $('#'+span).append(text.charAt(y));
+	    },randInt, i);
+	  };
+	  setTimeout(function(){
+	    $('#'+span).removeClass('cursor');
+	  },randInt+2500);
+	}
+
+	typeWrite('welcome');
+
 $('#ledon-button').click(function() {
-	$('#screen-text').text("This is text changed using jquery");
-	console.log('yas');
+	
+	typeWrite('screen-text');
+	onNewContent();
 
     $.ajax({
         type: 'POST',
         url: 'http://localhost:1337/LEDon'
     });
 });
-
+});
